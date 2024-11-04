5d15f21340f414251c8678f59c8b3f6dd29a3802
ricardopra <ricardopra@yahoo.com.br>
2024-11-04 14:07:28 -0300

    1.0

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
