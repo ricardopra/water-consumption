c385ff33da42dab57e48411ef1d3e51238249b22
ricardopra <ricardopra@yahoo.com.br>
2024-11-18 16:51:04 -0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,13 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Configuração detalhada do CORS
+  
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://water-consumption-pied.vercel.app/'], // Origens permitidas
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://water-consumption-pied.vercel.app/'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Permite credenciais (cookies, headers de autenticação)
-    maxAge: 3600 // Cache do preflight por 1 hora
+    credentials: true, 
+    maxAge: 3600 
   });
 
   app.use(express.static(join(__dirname, '..', 'public')));
